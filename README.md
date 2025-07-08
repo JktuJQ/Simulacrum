5fd1fcd9e0459de31f999bd885288cbc65958c39
JktuJQ <odmamontov@gmail.com>
2025-07-08 13:59:11 +0300

    Basic backend

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Simulacrum
+
+`Simulacrum` is a decentralized peer lending platform
+that will simplify the process of issuing and receiving loans between users without intermediaries.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use axum::{routing::get, Router};
 
+mod routes;
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(routes::index));
+
+    Ok(router.into())
 }
