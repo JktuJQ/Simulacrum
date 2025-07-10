233aec665c98f3053895f2a5b74949578cc1cc55
WonderWaffle4 <zhmelev.g@gmail.com>
2025-07-10 16:33:08 +0300

    MetaMask auth beta

--- static/js/base.js
+++ static/js/base.js
@@ -7,49 +7,90 @@ window.addEventListener('scroll', function() {
     }
 });
 
-walletConnected = false;
-const walletButton = document.getElementById('walletButton');
 
-walletButton.addEventListener('click', async () => {
-    if (!walletConnected && window.ethereum) {
-        try {
-            walletButton.classList.remove('btn-primary');
-            walletButton.classList.add('btn-secondary');
-            walletButton.disabled = true;
-            const web3 = new Web3(window.ethereum);
-            // request accounts from MetaMask
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            // get list of accounts
-            const accounts = await web3.eth.getAccounts();
-            // get the first account and populate placeholder
-            let address = accounts[0]
-
-            let get_token = await fetch("http://localhost:8000/register", {
-                method: "POST",
-                body: address,
-                headers: {
-                    "Content-type": "raw; charset=UTF-8"
-                }
-            });
-
-            console.log(get_token);
-
-            walletButton.innerText = address;
-            walletConnected = true;
-        } catch (err) {
-            walletButton.classList.remove('btn-secondary');
-            //walletButton.classList.add('btn-primary');
-        }
-        walletButton.disabled = false;
-    } else if (walletConnected) {
-        
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
     }
-    else {
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function walletButtonToggle() {
+    if ('btn-primary' in walletButton.classList) {
+        walletButton.classList.remove('btn-primary');
+        walletButton.classList.add('btn-secondary');
+        walletButton.disabled = true;
+    } else {
+        walletButton.classList.remove('btn-secondary');
+        walletButton.classList.add('btn-primary');
+        walletButton.disabled = false;
+    }
+}
+
+async function isWalletConnected() {
+    walletConnected = getCookie('walletConnected');
+    if (walletConnected) {
+        try {
+            await auth_metamask();
+            return true;
+        } catch {
+            setCookie('walletConnected', false, 100);
+        }
+        return false;
+    }
+}
+
+async function auth_metamask() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+}
+
+async function connectWallet(walletButton) {
+    if (!window.ethereum) {
         // no Ethereum provider - instruct user to install MetaMask
         document.getElementById('warn').innerHTML =
             "Please <a href='https://metamask.io/download/'>install MetaMask</a>.";
-        document.getElementById('requestAccounts').disabled = true;
+        return;
     }
+
+    if (await isWalletConnected()) {
+        return;
+    }
+
+    try {
+
+        const web3 = new Web3(window.ethereum);
+
+        // request accounts from MetaMask
+        await auth_metamask();
+        // // get list of accounts
+        const accounts = await web3.eth.getAccounts();
+        let address = accounts[0]
+
+        walletButton.innerText = address;
+        setCookie('walletConnected', true, 5);
+    } catch (err) {
+        console.log(err);
+    }
+    walletButtonToggle(walletButton);
+}
+
+
+walletButton.addEventListener('click', async () => {
+    
 });
 
 const mobileMenuButton = document.getElementById('mobile-menu-button');
